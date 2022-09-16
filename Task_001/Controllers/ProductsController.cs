092c9c92494f6d36581bb45b56b6069ebff66cb4
Andrii Korshenko <andriikorshenko@gmail.com>
2022-09-16 15:28:13 +0300

    Add project files.

---
+++ Task_001/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task_001.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private Warehouse? _warehouse;
+        private List<Warehouse>? _inventory;        
+
+        public ProductsController()
+        {
+            _warehouse = new();
+            _inventory = _warehouse.ReadAsJsonFormat<List<Warehouse>>("warehouse.json");
+        }
+
+        [Route("")]
+        [Route("products/list/{category?}")]
+        public IActionResult List(string? category)
+        {
+            if (category == "pc")
+            {
+                var linq = from p in _inventory where p.Category == "PC" select p;
+                return View(linq as object);
+            }
+            else if (category == "office")
+            {
+                var linq = from p in _inventory where p.Category == "Office" select p;
+                return View(linq as object);
+            }
+            return View(_inventory as object);
+        }        
+    }
+}
---
+++ Task_001/Warehouse.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+
+namespace Task_001
+{
+    public class Warehouse
+    {
+        public string? ProductName { get; set; }
+        public int Quontity { get; set; }
+        public uint Price { get; set; }
+        public string? Category { get; set; }
+
+        public void SerializeAsJson(string fileName)
+        {
+            JsonSerializerOptions options = new()
+            {
+                WriteIndented = true
+            };
+
+            List<Warehouse> inventory = new()
+            {
+                new Warehouse { ProductName = "Dell 3321F", Quontity = 12, Price = 32_000, Category = "PC" },
+                new Warehouse { ProductName = "Apple MacBook Pro", Quontity = 12, Price = 51_000, Category = "PC" },
+                new Warehouse { ProductName = "Razor Mouse 12A", Quontity = 12, Price = 1000, Category = "Office" },
+                new Warehouse { ProductName = "ASUS Keys 88", Quontity = 12, Price = 425, Category = "Office" },
+                new Warehouse { ProductName = "Apple Pen", Quontity = 12, Price = 225, Category = "Office" }
+            };
+
+            File.WriteAllText(fileName, JsonSerializer.Serialize(inventory, options));
+        }
+
+        public T? ReadAsJsonFormat<T>(string fileName)
+        {
+            return JsonSerializer.Deserialize<T>(File.ReadAllText(fileName));
+        }
+    }
+}
