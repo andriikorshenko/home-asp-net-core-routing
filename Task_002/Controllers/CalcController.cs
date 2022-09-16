c428d2e31cffe27d84bbadc16652bea43cfe334d
Andrii Korshenko <andriikorshenko@gmail.com>
2022-09-16 15:58:16 +0300

    added task_002

---
+++ Task_002/Controllers/CalcController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task_002.Controllers
+{
+    public class CalcController : Controller
+    {
+        public IActionResult Add(int x, int y)
+            => View("Result", x + y);
+        
+
+        public IActionResult Mul(int x, int y)        
+            => View("Result", x * y);
+        
+
+        public IActionResult Div(int x, int y)        
+            => y == 0 ? BadRequest("Divide by zero!") : View("Result", x / y);
+        
+
+        public IActionResult Sub(int x, int y)        
+            => View("Result", x - y);        
+    }
+}
